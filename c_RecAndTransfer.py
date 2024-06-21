9e08f36ff5494a49bdaa9765f86ef7e949179be5
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2024-06-21 14:17:04 +0800

    [add][class][by num]

--- c_RecAndTransfer.py
+++ c_RecAndTransfer.py
@@ -1,6 +1,28 @@
 # Edge Impulse - OpenMV Image Classification Example
 
+# 说明：来自https://edgeimpulse.com/ 的 vangaovh/nuble_again 项目中 deployment V3
+
 import sensor, image, time, os, tf, uos, gc
+import network
+import time
+import userfunc
+
+SSID = "rtthread"  # Network SSID
+KEY = "12345678"  # Network key
+
+# Init wlan module and connect to network
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(SSID, KEY)
+
+while not wlan.isconnected():
+    print('Trying to connect to "{:s}"...'.format(SSID))
+    time.sleep_ms(1000)
+
+# We should have a valid IP now via DHCP
+print("WiFi Connected ", wlan.ifconfig())
+
+userfunc.mqtt_start()
 
 sensor.reset()                         # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565 (or GRAYSCALE)
@@ -23,6 +45,8 @@ try:
 except Exception as e:
     raise Exception('Failed to load "labels.txt", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
 
+found_0_count = 0
+found_2_count = 0
 clock = time.clock()
 while(True):
     clock.tick()
@@ -38,5 +62,25 @@ while(True):
 
         for i in range(len(predictions_list)):
             print("%s = %f" % (predictions_list[i][0], predictions_list[i][1]))
+
+            if (predictions_list[i][0] == '0') & (predictions_list[i][1] > 0.9) :
+                continue_data_0 = continue_data_0 + 1
+                if continue_data_0 > 10:
+                    continue_data_0 = 0
+                    found_0_count = found_0_count +1
+                    found_0_str = str(found_0_count)
+                    ret = userfunc.mqtt_publish('found 0 times = '+ found_0_str)
+            elif (predictions_list[i][0] == '0') & (predictions_list[i][1] < 0.9):
+                continue_data_0 = 0
+            
+            if (predictions_list[i][0] == '2') & (predictions_list[i][1] > 0.9) :
+                continue_data_2 = continue_data_2 + 1
+                if continue_data_2 > 10:
+                    continue_data_2 = 0
+                    found_2_count = found_2_count +1
+                    found_2_str = str(found_2_count)
+                    ret = userfunc.mqtt_publish('found 2 times = '+ found_2_str)
+            elif (predictions_list[i][0] == '2') & (predictions_list[i][1] < 0.9):
+                continue_data_2 = 0
 
     print(clock.fps(), "fps")
--- mqtt_publish.py
+++
@@ -1,8 +0,0 @@
-import userfunc
-import time
-
-while True:
-    time.sleep_ms(1000)
-    ret = userfunc.mqtt_publish('tsatet')
-
-
