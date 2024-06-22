0f9a3a983d8356b75cdb5ca591811c21c68f50a1
Gaogao <gf1024355591@163.com>
2024-06-22 20:18:19 +0800

    [add][cat][discover]

---
+++ d_cat_discover.py
@@ -0,0 +1,87 @@
+# Edge Impulse - OpenMV Object Detection Example
+
+import sensor, image, time, os, tf, math, uos, gc
+import network
+import time
+import userfunc
+
+SSID = "rtthread"  # Network SSID
+KEY = "12345678"  # Network key
+
+# Init wlan module and connect to network
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(SSID, KEY)
+
+while not wlan.isconnected():
+    print('Trying to connect to "{:s}"...'.format(SSID))
+    time.sleep_ms(1000)
+
+# We should have a valid IP now via DHCP
+print("WiFi Connected ", wlan.ifconfig())
+
+userfunc.mqtt_start()
+
+sensor.reset()                         # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
+sensor.set_windowing((240, 240))       # Set 240x240 window.
+sensor.skip_frames(time=2000)          # Let the camera adjust.
+
+net = None
+labels = None
+min_confidence = 0.5
+
+try:
+    # load the model, alloc the model file on the heap if we have at least 64K free after loading
+    net = tf.load("trained.tflite", load_to_fb=uos.stat('trained.tflite')[6] > (gc.mem_free() - (64*1024)))
+except Exception as e:
+    raise Exception('Failed to load "trained.tflite", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
+
+try:
+    labels = [line.rstrip('\n') for line in open("labels.txt")]
+except Exception as e:
+    raise Exception('Failed to load "labels.txt", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
+
+colors = [ # Add more colors if you are detecting more than 7 types of classes at once.
+    (255,   0,   0),
+    (  0, 255,   0),
+    (255, 255,   0),
+    (  0,   0, 255),
+    (255,   0, 255),
+    (  0, 255, 255),
+    (255, 255, 255),
+]
+
+discover_count = 0
+send_times = 0
+clock = time.clock()
+while(True):
+    clock.tick()
+
+    img = sensor.snapshot()
+
+    # detect() returns all objects found in the image (splitted out per class already)
+    # we skip class index 0, as that is the background, and then draw circles of the center
+    # of our objects
+
+    for i, detection_list in enumerate(net.detect(img, thresholds=[(math.ceil(min_confidence * 255), 255)])):
+        if (i == 0): continue # background class
+        if (len(detection_list) == 0): continue # no detections for this class?
+
+        print("********** %s **********" % labels[i])
+        for d in detection_list:
+            [x, y, w, h] = d.rect()
+            center_x = math.floor(x + (w / 2))
+            center_y = math.floor(y + (h / 2))
+            print('x %d\ty %d' % (center_x, center_y))
+            img.draw_circle((center_x, center_y, 12), color=colors[i], thickness=2)
+            discover_count = discover_count +1
+            if discover_count > 10:
+                discover_count = 0
+                send_times = send_times + 1
+                send_times_str = str(send_times)
+                userfunc.mqtt_publish('discover the cat,this is '+ send_times_str + ' times')
+
+
+    print(clock.fps(), "fps", end="\n\n")
